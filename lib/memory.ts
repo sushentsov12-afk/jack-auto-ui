5a76619dd1692953fcd9c71f9b0a3d775cd2c24e
Alex <sushentsov12@gmail.com>
2026-06-04 20:00:49 +0300

    Update memory.ts

--- lib/memory.ts
+++ lib/memory.ts
@@ -14,3 +14,19 @@ export function addMemory(item: Item) {
 export function getMemory() {
   return memory;
 }
+type Item = {
+  symptom: string;
+  diagnosis: string;
+  time: number;
+};
+
+const memory: Item[] = [];
+
+export function addMemory(item: Item) {
+  memory.push(item);
+  if (memory.length > 20) memory.shift();
+}
+
+export function getMemory() {
+  return memory;
+}
