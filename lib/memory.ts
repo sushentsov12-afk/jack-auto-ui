2b6fa9aac92928f18bff60e693fcf7b31efee058
Alex <sushentsov12@gmail.com>
2026-06-04 19:53:24 +0300

    Create memory.ts

---
+++ lib/memory.ts
@@ -0,0 +1,16 @@
+type Item = {
+  symptom: string;
+  diagnosis: string;
+  time: number;
+};
+
+const memory: Item[] = [];
+
+export function addMemory(item: Item) {
+  memory.push(item);
+  if (memory.length > 20) memory.shift();
+}
+
+export function getMemory() {
+  return memory;
+}
